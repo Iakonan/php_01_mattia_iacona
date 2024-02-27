880c00d9abfc9406ff211a50cac7552c0b427c8b
Mattia Iacona <mattiaiacona87@icloud.com>
2024-02-27 19:30:52 +0100

    primo push

---
+++ php1.php
@@ -0,0 +1,25 @@
+
+<?php
+
+// $integer = 2;
+// $float = 2.5;
+// $string = "ciao come stai?";
+// $boolean = true;
+
+// var_dump($integer);
+// var_dump($float);
+// var_dump($string);
+// var_dump($boolean);
+
+
+// const INTEGER = 2;
+// const FLOAT = 2.5;
+// const STRING = "ciao come stai?";
+// const BOOLEAN = true;
+
+
+
+
+
+
+
